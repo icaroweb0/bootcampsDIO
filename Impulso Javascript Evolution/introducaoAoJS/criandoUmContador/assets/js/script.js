c7c021f27cac174527fbcabbab30cb682a895ae5
Icaro Siqueira Pinto <70227039+icaroweb0@users.noreply.github.com>
2022-07-21 13:18:33 -0300

    chalange updated

--- Impulso Javascript Evolution/introducaoAoJS/criandoUmContador/assets/js/script.js
+++ Impulso Javascript Evolution/introducaoAoJS/criandoUmContador/assets/js/script.js
@@ -2,14 +2,32 @@ var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0
 
 function increment() {
-    currentNumber++
-    currentNumberWrapper.innerHTML = currentNumber;
+    
+    if (currentNumber<10) {
+        currentNumber++
+        currentNumberWrapper.innerHTML = currentNumber;
+        if (currentNumber < 0) {
+            document.querySelectorAll('span')[0].style.color = 'red';
+        } else {
+            document.querySelectorAll('span')[0].style.color = ''
+        }
+    }
+    
 }
 
 function decrement() {
+
+
     currentNumber--
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0) {
+        document.querySelectorAll('span')[0].style.color = 'red';
+    } else {
+        document.querySelectorAll('span')[0].style.color = ''
+    }
+    
 }
+
 
 
 // >>>> DARK THEME <<<<
@@ -20,6 +38,8 @@ let icon = document.getElementById('theme');
     icon.innerHTML = '<i class="fas fa-moon dark" onclick="switchToMoon()"></i>';
     document.body.style.color = 'rgb(26, 58, 75)' 
     document.body.style.backgroundColor = 'white';
+    document.querySelectorAll('button')[0].style.backgroundColor = 'white';
+    document.querySelectorAll('button')[1].style.backgroundColor = 'white';
 }
 
 //changes Icon To Moon
@@ -28,5 +48,6 @@ function switchToMoon() {
         icon.innerHTML = '<i class="fas fa-sun light" onclick="switchToSun()"></i>';
         document.body.style.backgroundColor = '';
         document.body.style.color = '' 
-
+        document.querySelectorAll('button')[0].style.backgroundColor = '';
+        document.querySelectorAll('button')[1].style.backgroundColor = '';
     }

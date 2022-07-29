f907d5b68935054b64b1996e08f84add11359624
Icaro Siqueira Pinto <70227039+icaroweb0@users.noreply.github.com>
2022-07-29 02:34:02 -0300

    Exercise update

--- Impulso Javascript Evolution/dominandoJS/variaveisETipos/script.js
+++ Impulso Javascript Evolution/dominandoJS/variaveisETipos/script.js
@@ -1,4 +1,5 @@
 
+//Solução 1
 function verificaPalindromo(palavra){
     string = palavra
     stringRev = palavra.split("").reverse("").join("")
@@ -8,6 +9,25 @@ function verificaPalindromo(palavra){
     } else {
         (string == stringRev) ? console.log(`${string} é um palíndromo` ): console.log(`${string} não é um palíndromo`)
     }
-}
+} 
 
 verificaPalindromo("ojo oko ojo")
+
+//Solução 2
+let resultado
+function verificaPalindromo2(palavra) {
+    if (!palavra) {
+        return "Não é uma palavra válida!"
+    }
+
+    for (i = 0 ; i < palavra.length ; i++){
+        if (palavra[i] == palavra[palavra.length -1 -i]) {
+            return resultado = true  
+        }
+    }
+}
+
+verificaPalindromo2("omo");
+
+(resultado) ? console.log(`é um palíndromo`) : console.log(`não é um palíndromo`)
+
